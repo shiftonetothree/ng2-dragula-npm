3c20be3c8700dbdc24175f6bdb16a65ef757a0fc
JamesZhang <shiftonetothree@126.com>
2018-08-22 17:59:12 +0800

    首次提交

---
+++ components/dragula.class.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var dragulaExpt = require("dragula");
+exports.dragula = dragulaExpt.default || dragulaExpt;
---
+++ ng2-dragula.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var index_1 = require("./index");
+exports.dragula = index_1.dragula;
+exports.DragulaDirective = index_1.DragulaDirective;
+exports.DragulaModule = index_1.DragulaModule;
+exports.DragulaService = index_1.DragulaService;
